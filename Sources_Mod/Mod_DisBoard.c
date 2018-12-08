f3fedeba4136fc4eca6d778d679d7f66fa7555dc
wjb1991 <wang_jiang_bo@163.com>
2018-12-08 20:34:11 +0800

    整机集成 紫外能够显示透过率了
    但是USB又连接不上了

--- Sources_Mod/Mod_DisBoard.c
+++ Sources_Mod/Mod_DisBoard.c
@@ -18,18 +18,28 @@ DisBoard_t st_DisBoard= {
 
 INT8U HexToBcd(INT8U uch_Hex)
 {
-    INT8U uch_Bcd = (uch_Hex/10*16) + (uch_Hex % 10);
+    INT8U uch_Bcd;
+    if(uch_Hex > 99)
+        uch_Hex = 99;
+    uch_Bcd = (uch_Hex/10*16) + (uch_Hex % 10);
     return uch_Bcd;
 }
 
 
 void Mod_DisBoardPoll(void)
 {
+    OS_ERR os_err;
     INT8U auch_Buff[3];
     auch_Buff[0] = st_DisBoard.uch_DisUnit1++;
-    auch_Buff[1] = st_DisBoard.uch_DisUnit2;
+    auch_Buff[1] = st_GasMeasure.f_Trans;
     auch_Buff[2] = st_Grey.f_Trans;             //0-100
     
+    auch_Buff[1] = HexToBcd(auch_Buff[1]);
     auch_Buff[2] = HexToBcd(auch_Buff[2]);
     Mod_StdbusWriteCmd(&st_StdbusDis,0x10,auch_Buff,3);
+
+    OSTimeDlyHMSM(0u, 0u, 0u, 500u,
+          OS_OPT_TIME_HMSM_STRICT,       /* 周期模式 */
+          &os_err);
+      
 }
--- Sources_Task/Task_DisBoard.c
+++ Sources_Task/Task_DisBoard.c
@@ -5,10 +5,6 @@ void Task_DisBoard (void *p_arg)
     OS_ERR os_err;
     while(1)
     {
-        OSTimeDlyHMSM(0u, 0u, 0u, 500u,
-              OS_OPT_TIME_HMSM_STRICT,       /* 周期模式 */
-              &os_err);
-      
-        //Mod_DisBoardPoll();
+        Mod_DisBoardPoll();
     }
 }
